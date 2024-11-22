736a483abb2ad43f5dfb9458da8595038a9a54ca
Neda Vaheb <n.vaheb.nv@gmail.com>
2024-11-22 08:42:31 -0800

    Layout was created and add to aap.js

---
+++ crm/components/layout/Layout.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+function Layout({children}) {
+  return (
+    <>
+      <header className="header">
+
+<h2>BOTCAMP CRM</h2>
+
+       
+        <Link href="/add-customer">Add Customer</Link>
+
+      </header>
+      <div className="main">{children}</div>
+      <footer className="footer">
+      
+        Next.js course | CRM Project &copy;
+      </footer>
+    </>
+  )
+}
+
+export default Layout
