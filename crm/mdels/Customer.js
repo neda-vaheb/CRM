cc5b92ca399221638a45f9fa7971616ca252e1d5
Neda Vaheb <n.vaheb.nv@gmail.com>
2024-11-22 09:27:33 -0800

    Schema and model was created

--- crm/mdels/Customer.js
+++ crm/mdels/Customer.js
@@ -1,4 +1,35 @@
-import {Schema,model,models} from "mongoose";
-const custumerSchema = ()=>{
-    
-}
+import { Schema, model, models } from "mongoose";
+const custumerSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 1,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    minLength: 1,
+  },
+  email: {
+    type: String,
+    required: true,
+    minLength: 1,
+  },
+  phone: String,
+  address: String,
+  postalCode: Number,
+  date: Date,
+  products: {
+    type: Array,
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: () => Date.now(),
+    immutable: true,
+  },
+  updatedAt: { type: Date, default: () => Date.now() },
+});
+const Customer =  models.Customer || model("Customer", custumerSchema);
+
+export default Customer
